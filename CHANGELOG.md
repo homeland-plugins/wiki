b7805a1b0530312a2086f6aac24f91a0bc35b229
Jason Lee <huacnlee@gmail.com>
2017-03-07 10:17:25 +0800

    Version 0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+0.2.0
+-----
+
+- First release.
+- Include Only Wiki features from Ruby China.
--- lib/homeland/wiki/version.rb
+++ lib/homeland/wiki/version.rb
@@ -2,6 +2,6 @@ module Homeland
   module Wiki
     NAME    = 'wiki'
     DESCRIPTION = 'Display Wiki channel in navbar for wike pages.'
-    VERSION = '0.0.2'
+    VERSION = '0.2.0'
   end
 end
