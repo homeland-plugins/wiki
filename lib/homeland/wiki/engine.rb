e0d51832a719e8ebfc932cacda9c6fb4ba369ebc
Jason Lee <huacnlee@gmail.com>
2019-12-23 17:32:36 +0800

    Fix assets.precompile

--- lib/homeland/wiki/engine.rb
+++ lib/homeland/wiki/engine.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 module Homeland
   module Wiki
     class Engine < ::Rails::Engine
       isolate_namespace Homeland::Wiki
+
+      initializer 'homeland.wiki.assets.precompile', group: :all do |app|
+        app.config.assets.precompile += %w[homeland/wiki/application.css homeland/wiki/application.js]
+      end
 
       initializer 'homeland.wiki.init' do |app|
         if Setting.has_module?(:wiki)
@@ -12,8 +18,8 @@ module Homeland
             plugin.version           = Homeland::Wiki::VERSION
             plugin.navbar_link       = true
             plugin.user_menu_link    = false
-            plugin.root_path         = "/wiki"
-            plugin.admin_path        = "/admin/wiki"
+            plugin.root_path         = '/wiki'
+            plugin.admin_path        = '/admin/wiki'
             plugin.admin_navbar_link = true
             plugin.spec_path         = config.root.join('spec')
           end
@@ -22,7 +28,7 @@ module Homeland
             mount Homeland::Wiki::Engine, at: '/'
           end
 
-          app.config.paths["db/migrate"].concat(config.paths["db/migrate"].expanded)
+          app.config.paths['db/migrate'].concat(config.paths['db/migrate'].expanded)
         end
       end
     end
--- lib/homeland/wiki/version.rb
+++ lib/homeland/wiki/version.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module Homeland
   module Wiki
     NAME = 'wiki'
     DESCRIPTION = 'Display Wiki channel in navbar for wike pages.'
-    VERSION = '0.4.3'
+    VERSION = '0.4.4'
   end
 end
